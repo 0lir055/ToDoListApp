34fb5b16c2ddb43531b2d837c8e9ec8d1ab85635
Oli <olirossxiv@gmail.com>
2025-08-26 22:12:35 +0100

    Completed methods within the Task class and added in getters and setters

--- src/main/java/com/todolist/Task.java
+++ src/main/java/com/todolist/Task.java
@@ -1,5 +1,28 @@
 package com.todolist;
 
 public class Task {
-    
+    private String description;
+    private boolean isCompleted;
+
+    public Task(String description) {
+        this.description = description;
+        this.isCompleted = false;
+    }
+
+    public String getDescription(){
+        return description;
+    }
+
+    public boolean isCompleted(){
+        return isCompleted;
+    }
+
+    public void taskCompleted(){
+        isCompleted = true;
+    }
+
+    @Override
+    public String toString(){
+        return (isCompleted ? "[ X ]" : "[ ]") + description;
+    }
 }
